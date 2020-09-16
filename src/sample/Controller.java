50ddb4e677221a356153f5021522a343ce1beba9
joanjuz <62675444+joanjuz@users.noreply.github.com>
2020-09-16 17:57:25 -0600

    Enviar mensajes simples (sin sockets, solo interfaz)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,25 +2,23 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
 import java.util.ArrayList;
 
 
 public class Controller{
     public TextField Caja;
-    public Label Chat;
-    ArrayList<String> messages = new ArrayList<String>();
+    public TextArea Chat;
+    String messages = "";
 
-    public void generateRandom(ActionEvent event){
-        String message = Caja.getText();
-        messages.add(message+"\n");
-        int contador = 0;
-        while (contador < messages.size()){
-            contador += 1;
-            Chat.setText(messages.get(contador));
-            //t
-        }
+    public void send(ActionEvent event){
+        String message = Caja.getText() + "\n";
+        messages += message;
+        Chat.setText(messages);
+        Caja.setText("");
+
+
 
 
     }
