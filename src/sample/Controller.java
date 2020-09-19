e53f9d5a4beea36562ff9d226fc9b23225965aec
joanjuz <62675444+joanjuz@users.noreply.github.com>
2020-09-18 22:25:37 -0600

    Clase Send_Message

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,29 +1,18 @@
 package sample;
 
-import com.sun.javafx.stage.EmbeddedWindow;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import java.util.ArrayList;
 
 
 public class Controller{
-    public TextField Caja;
-    public TextArea Chat;
+    protected TextField Caja;
+    protected TextArea Chat;
 
 
 
 
     public void send_(ActionEvent event){
         Send_Message.send(Caja,Chat);
-
-
-
-
-
     }
-
-
 }
--- src/sample/Send_Message.java
+++ src/sample/Send_Message.java
@@ -3,8 +3,6 @@ package sample;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import java.awt.*;
-
 public class Send_Message{
     static String messages = "";
     public static void send(TextField Caja, TextArea Chat){
